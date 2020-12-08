2bb1a78c064e9252df713825ceda08ecbaf20ee5
Sam-Ayn <samueleayn@gmail.com>
2020-12-08 22:11:17 +0300

    change path, add page size

--- src/main/java/ru/springcourse/lessons/springbootcruddemo/repository/EmployeeRepository.java
+++ src/main/java/ru/springcourse/lessons/springbootcruddemo/repository/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package ru.springcourse.lessons.springbootcruddemo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.springcourse.lessons.springbootcruddemo.entitiy.Employee;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
